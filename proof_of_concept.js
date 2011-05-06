ab2f420ef6fe46ffc4a02370af911d087529adbd
C Püh <cpuehringer@gmx.at>
2011-05-06 22:09:18 +0200

    just testing

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -1,5 +1,12 @@
 
+var enabled=true
 document.onmousedown=function(event) {
-	if (event.button == 1)
-		alert(event.type)
+	if(enabled) {
+		if(event.button==1)
+			alert(event.type)
+	}
+}
+document.onkeypress=function(event) {
+	if(event.keyCode==27):
+		enabled=false
 }
