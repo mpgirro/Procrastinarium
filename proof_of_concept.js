b0e0a12c161fa332cc4bbc5f75a8707d87ae9c4b
C Püh <cpuehringer@gmx.at>
2011-05-06 22:14:08 +0200

    again

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -1,9 +1,9 @@
 
 var enabled=true
 document.onmousedown=function(event) {
-	if(enabled) {
-		alert(event.button)
-	}
+	//if(enabled) {
+		alert(enabled + ":" + event.button)
+	//}
 }
 document.onkeypress=function(event) {
 	if(event.keyCode==27):
