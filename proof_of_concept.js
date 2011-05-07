e1865b9313c590c6138be96e31bc39e5754f3927
C Püh <cpuehringer@gmx.at>
2011-05-07 19:22:59 +0200

    wohooo

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -1,23 +1,50 @@
-
 window.focus()
 var enabled=true
-document.onmousedown=function(event) {
-	event=event || window.event;
+var m_down_element=null
+var m_x=0
+var m_y=0
+document.onmousedown=function(event) {		//MOUSE DOWN
 	if(enabled){
+		event=event || window.event;
 		if(event.button==0){
-			alert(event.clientX + " " + event.clientY);
+			m_down_element=event.target;
+			m_x=event.clientX;
+			m_y=event.clientY;
 		}
-	}
-	window.focus()
+		window.focus();			//by calling the alert function, the current window looses focus and no more keydown events would be possible
+	}			
 };
+
+document.onmouseup=function(event) {		//MOUSE UP
+	if(enabled){
+		event=event || window.event;
+		if(event.button==0){
+			m_down_element=null;
+		}
+		window.focus();			//by calling the alert function, the current window looses focus and no more keydown events would be possible
+	}	
+};
+
+document.onmousemove=function(event) {		//MOUSE MOVE
+	if(enabled && m_down_element){
+		event=event || window.event;
+		var mX=event.clientX;
+		var mY=event.clientY;
+		alert((mX-m_x) + " " + (mY-m_y)); 
+		m_x=mX;
+		m_y=mY;
+		window.focus();			//by calling the alert function, the current window looses focus and no more keydown events would be possible
+	}	
+};
+
 document.onkeydown=function(event) {
 	alert("keydown");
 	event=event || window.event;
 	var k;
-	if(window.event){ //IE
+	if(window.event){ 			//IE
 		k=event.keyCode;
 	}
-	else if(event.which){ // Netscape/Firefox/Opera
+	else if(event.which){ 			// Netscape/Firefox/Opera
 		k=event.which;
 	}else{
 		k=0
@@ -25,5 +52,5 @@ document.onkeydown=function(event) {
 	if(k==27){
 		enabled = !enabled
 	}
-	window.focus()
+	window.focus()				//by calling the alert function, the current window looses focus and no more keydown events would be possible
 };
