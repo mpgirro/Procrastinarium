ab428ac7a32694ff5bd8e9c2978d818828af5300
C Püh <cpuehringer@gmx.at>
2011-05-07 18:16:20 +0200

    ...

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -9,17 +9,17 @@ document.onmousedown=function(event) {
 }
 document.onkeydown=function(event) {
 	alert("keypress")
-	var k;
-	if(window.event){ //IE
-		k=e.keyCode;
-	}
-	else if(e.which){ // Netscape/Firefox/Opera
-		k=e.which;
-	}else{
-		k=0
-	}
-	if(k==27){
-		alert("escape")
-		enabled = !enabled
-	}
+	//var k;
+	//if(window.event){ //IE
+	//	k=e.keyCode;
+	//}
+	//else if(e.which){ // Netscape/Firefox/Opera
+	//	k=e.which;
+	//}else{
+	//	k=0
+	//}
+	//if(k==27){
+	//	alert("escape")
+	//	enabled = !enabled
+	//}
 }
