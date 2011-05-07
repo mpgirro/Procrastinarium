70e037274a72454717f3463297813942e4a2170d
C Püh <cpuehringer@gmx.at>
2011-05-07 18:32:10 +0200

    ...

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -1,16 +1,18 @@
 
 var enabled=true
 document.onmousedown=function(event) {
-	var e=window.event || e
+	var e=window.event || event;
 	if(enabled){
 		if(e.button==0){
-			alert(e.clientX + " " + e.clientY)
+			alert("yay");
+			alert(e.clientX + " " + e.clientY);
 		}
 	}
 }
 document.onkeydown=function(event) {
-	alert("keypress")
-	var e=window.event || event
+	alert("keypress");
+	var e=window.event || event;
+	alert("after")
 	//var k;
 	//if(window.event){ //IE
 	//	k=e.keyCode;
