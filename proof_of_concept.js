a832b2b69565d481887d23fdd4f4b10021957879
C Püh <cpuehringer@gmx.at>
2011-05-07 19:33:31 +0200

    ...

--- proof_of_concept.js
+++ proof_of_concept.js
@@ -2,24 +2,27 @@ window.focus()
 var enabled=true
 var m_down=false
 var m_down_element=null
-var m_x=0
-var m_y=0
+var last_x=0
+var last_y=0
 document.onmousedown=function(event) {		//MOUSE DOWN
 	if(enabled){
 		event=event || window.event;
 		if(event.button==0){
 			m_down=true
 			m_down_element=event.target;
-			m_x=event.clientX;
-			m_y=event.clientY;
+			last_x=event.clientX;
+			last_y=event.clientY;
 		}
 		window.focus();			//by calling the alert function, the current window looses focus and no more keydown events would be possible
 	}			
 };
 
 document.onmouseup=function(event) {		//MOUSE UP
-	m_down=false
-	m_down_element=null;
+	vent=event || window.event;
+	if(event.button==0){
+		m_down=false;
+		m_down_element=null;
+	}
 };
 
 document.onmousemove=function(event) {		//MOUSE MOVE
@@ -27,9 +30,9 @@ document.onmousemove=function(event) {		//MOUSE MOVE
 		event=event || window.event;
 		var mX=event.clientX;
 		var mY=event.clientY;
-		alert((mX-m_x) + " " + (mY-m_y)); 
-		m_x=mX;
-		m_y=mY;
+		alert((mX-last_x) + " " + (mY-last_y)); 
+		last_x=mX;
+		last_y=mY;
 		window.focus();			//by calling the alert function, the current window looses focus and no more keydown events would be possible
 	}	
 };
